07f004cef89a7e3628df2972569bf8eaa355150e
José Luis Bueno <74137433+jxtaaa@users.noreply.github.com>
2022-03-05 16:51:23 +0100

    Implementation of Fibonacci sequence done @ Fibonacci.java, added documentation on Fibonacci.java & FibonacciTest.java.

--- src/main/java/org/jlbp/fibonacci/Fibonacci.java
+++ src/main/java/org/jlbp/fibonacci/Fibonacci.java
@@ -1,5 +1,22 @@
 package org.jlbp.fibonacci;
 
-public class Fibonacci {
+/**
+ * Class providing a method that computes
+ * the nth value of the Fibonacci sequence.
+ *
+ * @author José L. Bueno Pachón
+ */
 
+public class Fibonacci {
+    public int compute(int n) {
+        if (n > 1) {
+            return compute(n - 1) + compute(n - 2);
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 0) {
+            return 0;
+        } else {
+            throw new RuntimeException("Valor no válido: " + n);
+        }
+    }
 }
--- src/test/java/org/jlbp/fibonacci/FibonacciTest.java
+++ src/test/java/org/jlbp/fibonacci/FibonacciTest.java
@@ -1,6 +1,20 @@
 package org.jlbp.fibonacci;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Fibonacci sequence:
+ * Fib(0) = 0
+ * Fib(1) = 1
+ * Fib(2) = Fib(0) + Fib(1) = 0 + 1 = 1
+ * Fib(3) = Fib(1) + Fib(2) = 1 + 1 = 2
+ * Fib(4) = Fib(2) + Fib(3) = 1 + 2 = 3
+ *
+ */
 
 class FibonacciTest {
 
